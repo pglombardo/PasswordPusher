802b849bd98f2d5195decd02361c374ab0aa144e
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-01-03 00:18:25 +0100

    Re-org of tips and notes.  Use a constant for initial input text.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+PAYLOAD_INITIAL_TEXT = 'Enter the Password to be Shared'
+
+
+
 # Initialize the rails application
 PasswordPusher::Application.initialize!
