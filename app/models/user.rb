98ea0f952167f9a6077bfd928ea6173fbe50abca
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-01-13 18:24:23 +0100

    Fix missing comma

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :trackable, :validatable
-  devise :database_authenticatable
+  devise :database_authenticatable,
         :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
