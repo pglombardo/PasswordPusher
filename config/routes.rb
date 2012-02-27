22cde988da7c21ed78f7ea61b612ffc77cba2120
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-02-27 16:26:12 +0100

    Support omniauth.  Disable API for now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,28 @@
 PasswordPusher::Application.routes.draw do
   
+#  post "api/create"
+#  get "api/generate"
+#  get "api/list"
+#  get "api/config"
+
   mount RailsAdmin::Engine => '/power', :as => 'rails_admin'
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+    get "/login" => "devise/sessions#new"
+    delete "/logout" => "devise/sessions#destroy"
+    get "/register" => "devise/registrations#destroy"
+  end
 
-  resources :passwords, :only => [ :new, :create, :show ] do
+  resources :passwords, :only => [ :new, :create ] do
     resources :views, :only => [ :index, :show ]
   end
   
+  # Password paths
   match '/p/:url_token' => 'passwords#show'
   match '/p' => 'passwords#new'
+  
+  # The user dashboard
+#  match '/d' => 'dashboard#index'
 
   root :to => 'passwords#new'
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
