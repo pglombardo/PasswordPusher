5778691c72411e86901577fd1e13691ada8c6bc9
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-07-22 15:07:58 +0200

    Add the modernizr scripts to the precompile list.

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,9 @@ module PasswordPusher
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.initialize_on_precompile = false
+    config.assets.precompile += ['fd-slider.css', 'fd-slider.js']
+    
   end
 end
