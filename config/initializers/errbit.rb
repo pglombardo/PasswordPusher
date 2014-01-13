4bbb363b315c7db56d83ab46909a7515044cd381
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-01-13 16:31:14 +0100

    Conditionalize Airbrake initialization

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,36 @@
+# Require the hoptoad_notifier gem in your App.
+# ---------------------------------------------
+#
+# Rails 3 - In your Gemfile
+# gem 'airbrake'
+#
+# Rails 2 - In environment.rb
+# config.gem 'airbrake'
+#
+# Then add the following to config/initializers/errbit.rb
+# -------------------------------------------------------
+
+if defined?(Airbrake)
+  Airbrake.configure do |config|
+    config.api_key = '5892d9dcd0f1f79a192b0dc90f31e2c4'
+    config.host    = 'nameless-scrubland-8508.herokuapp.com'
+    config.port    = 443
+    config.secure  = config.port == 443
+  end
+end
+
+# Set up Javascript notifications
+# -------------------------------
+#
+# To receive notifications for javascript errors,
+# you should add <%= airbrake_javascript_notifier %> to the top of your layouts.
+#
+# Testing
+# -------
+#
+# Rails 2 - you'll need to vendor airbrake to get the rake tasks
+# rake gems:unpack GEM=airbrake
+#
+# Run:
+# rake airbrake:test
+# refresh this page
