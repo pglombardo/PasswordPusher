697fd462d4c12ea9642eba0165b29db91605a8a6
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-10-22 17:46:06 +0200

    Oboe reconnect only if on Heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,26 +1,26 @@
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
- 
+
 before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
-   
+
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
-   
-  defined?(::Oboe) and ::Oboe.disconnect!
+
+  defined?(::OboeHeroku) and ::Oboe.disconnect!
 end
- 
+
 after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
-   
+
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
-   
-  defined?(::Oboe) and ::Oboe.reconnect!
+
+  defined?(::OboeHeroku) and ::Oboe.reconnect!
 end
