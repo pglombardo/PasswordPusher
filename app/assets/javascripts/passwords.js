f6a2ccdb5c83592c854378c8d7cd7f63c7368805
smidgen <nolan@nolanching.com>
2015-01-18 16:30:48 -0500

    Add client-side Javascript encryption of passwords, in addition to the server-side encryption layer, to increase security.

--- app/assets/javascripts/passwords.js
+++ app/assets/javascripts/passwords.js
@@ -6,10 +6,10 @@ function saveExpirations()
 {
   days_value = document.getElementById("password_expire_after_days").value
   views_value = document.getElementById("password_expire_after_views").value
-  
+
   $.cookie('pwpush_days',  days_value, { expires: 365 });
   $.cookie('pwpush_views', views_value, { expires: 365 });
-  
+
   e = document.getElementById("cookie-save")
   e.innerHTML = "Saved!"
   return true;
@@ -18,23 +18,44 @@ function saveExpirations()
 $(document).ready(function() {
   days = $.cookie('pwpush_days');
   views = $.cookie('pwpush_views');
-  
+
   de = document.getElementById("password_expire_after_days")
   dr = document.getElementById("daysrange")
-  if (days) {
-    de.value = days
-    dr.innerHTML = days + " Days"
-  } else {
-    showDaysValue(de.value)
+  if (de && dr) {
+    if (days) {
+      de.value = days
+      dr.innerHTML = days + " Days"
+    } else {
+      showDaysValue(de.value)
+    }
   }
-  
+ 
   ve = document.getElementById("password_expire_after_views")
   vr = document.getElementById("viewsrange")
-  if (views) {
-    ve.value = views
-    vr.innerHTML = views + " Views"
-  } else {
-    showViewsValue(ve.value)
+  if (ve && vr) {
+    if (views) {
+      ve.value = views
+      vr.innerHTML = views + " Views"
+    } else {
+      showViewsValue(ve.value)
+    }
+  }
+
+  if ($('.payload.spoiler').length == 1) {
+	var crypttext = $('.payload.spoiler').text();
+	var pass64 = $.jStorage.get('pass64');
+	if (pass64 === null) {
+		pass64 = window.location.hash.substr(1);
+	}
+	window.location.hash = pass64;
+	if ($('#share_url') && $('#clip_data')) {
+		$('#share_url') .val(window.location);
+		$('#clip_data').text(window.location);
+	}
+	var passBits = sjcl.codec.base64.toBits(pass64);
+	$.jStorage.deleteKey('pass64');
+	var cleartext = sjcl.decrypt(passBits, crypttext);
+	$('.payload.spoiler').text(cleartext);
   }
 });
 
@@ -44,4 +65,15 @@ $('#password_payload').keypress(function() {
     $.noty.clearQueue()
     return false;
   }
+});
+
+$('form#new_password').submit(function() {
+	var cleartext = $('#password_payload').val();
+
+	if (cleartext) {
+		var passBits = sjcl.random.randomWords(4); /* 4 words = 4*4*8 = 128 bits = AES key size */
+		var pass64 = sjcl.codec.base64.fromBits(passBits);
+		$.jStorage.set('pass64', pass64, {ttl: 3000});
+		$('#password_payload').val(sjcl.encrypt(passBits, cleartext));
+	}
 });
