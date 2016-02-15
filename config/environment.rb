48d77518563fdf15c5b82d8c16333421b27d9fff
Peter Giacomo Lombardo <pglombardo@gmail.com>
2016-02-15 18:46:30 +0100

    Expanded documentation on new config vars.

--- config/environment.rb
+++ config/environment.rb
@@ -17,10 +17,26 @@ EXPIRE_AFTER_VIEWS_DEFAULT = 5
 EXPIRE_AFTER_VIEWS_MIN = 1
 EXPIRE_AFTER_VIEWS_MAX = 100
 
-# Are un-expired passwords deletable by viewers?
-DELETABLE_BY_VIEWER_DEFAULT = true
-# Offer passwords to optionally be deletable/undeletable?
-DELETABLE_BY_VIEWER_PASSWORDS = false
+# DELETABLE_BY_VIEWER_PASSWORDS
+# Can passwords be deleted by viewers?
+#
+# When true, passwords will have a link to optionally delete
+# the password being viewed.
+# When pushing a new password, this option will also add a
+# checkbox to conditionally enable/disable this feature on
+# a per-password basis.
+DELETABLE_BY_VIEWER_PASSWORDS = true
+
+# DELETABLE_BY_VIEWER_DEFAULT
+#
+# When the above option (DELETABLE_BY_VIEWER_PASSWORDS) is set to
+# true, this option does two things:
+#   1. Sets the default check state for the "Allow viewers to
+#       optionally delete password before expiration" checkbox
+#   2. Sets the default value for newly pushed passwords if
+#       if unspecified (such as with a json request)
+#
+DELETABLE_BY_VIEWER_DEFAULT = false
 
 # Initialize the rails application
 PasswordPusher::Application.initialize!
