0ffd4c9f59a329347914f9385a549990efb249d6
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-05-24 13:35:22 +0200

    Remove devise from user model and routes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :trackable, :validatable
-  devise :database_authenticatable, :omniauthable, :lockable,
-        :recoverable, :rememberable, :trackable, :validatable, :registerable
-
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  
-  def self.find_for_open_id(access_token, signed_in_resource=nil)
-    data = access_token.info
-    if user = User.where(:email => data["email"]).first
-      user
-    else
-      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
-    end
-  end
 end
