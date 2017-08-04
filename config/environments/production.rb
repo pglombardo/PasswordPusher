54589b21aee1ec87166f0aca89ab3f781712d637
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-08-04 20:53:19 +0200

    Production env services static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ PasswordPusher::Application.configure do
   config.cache_classes = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_files = false
+  config.serve_static_files = true
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
   config.assets.digest = true
