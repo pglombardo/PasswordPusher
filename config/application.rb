bfa33d91b8b92437e620913d9caf173d8e1ca2db
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2017-11-24 15:15:57 +0100

    Typing errors

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module PasswordPusher
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.before_configuration do
-	env_file = File.join(Rails.root, 'config', 'local_env.ymö')
+	env_file = File.join(Rails.root, 'config', 'local_env.yml')
 	YAML.load(File.open(env_file)).each do |key, value|
 	   ENV[key.to_s] = value
 	end if File.exists?(env_file)
