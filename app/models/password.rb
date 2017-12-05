e8d78bfcbd8c7bd5a8d8a51de8051ef8ec80501d
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2017-12-05 15:11:36 +0100

    Fix

--- app/models/password.rb
+++ app/models/password.rb
@@ -31,13 +31,13 @@ class Password < ActiveRecord::Base
     self.expire_after_time  ||= EXPIRE_AFTER_TIME_DEFAULT
     self.expire_after_views ||= EXPIRE_AFTER_VIEWS_DEFAULT
 
-    if EXPIRE_AFTER_TIME_MAX.to_i < 24
-      max_time = EXPIRE_AFTER_TIME_MAX.to_i
+    if EXPIRE_AFTER_TIME_MAX < 24
+      max_time = EXPIRE_AFTER_TIME_MAX
     else
-      max_time = (24*(EXPIRE_AFTER_TIME_MAX.to_i)-23).to_i
+      max_time = (24*(EXPIRE_AFTER_TIME_MAX-23))
     end if
 
-    unless self.expire_after_time.between?(EXPIRE_AFTER_TIME_MIN.to_i, max_time)
+    unless self.expire_after_time.between?(EXPIRE_AFTER_TIME_MIN, max_time)
       self.expire_after_time = EXPIRE_AFTER_TIME_DEFAULT
     end
 
