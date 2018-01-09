1e00c0ee63c3450bee048caa4e0f3ded54a58922
Leo Schoenborn <leonard.schoenborn@smarthouse.de>
2018-01-09 16:10:58 +0000

    No more cable

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
