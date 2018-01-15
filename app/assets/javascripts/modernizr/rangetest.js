29d20aa1a8b7c10e8edb478cd3c9526ecda2c31d
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-01-15 11:23:04 +0100

    new rangetest.js

--- app/assets/javascripts/modernizr/rangetest.js
+++ app/assets/javascripts/modernizr/rangetest.js
@@ -1,19 +1,14 @@
-Modernizr.load([
-{
-  // Test if Input Range is supported using Modernizr
-  test: Modernizr.inputtypes.range,
-  // If ranges are not supported, load the slider script and CSS file
-  nope: [
-    // The slider CSS file
-    "css!#{asset_path('fd-slider.css')}"
-    // Javascript file for slider
-    ,"#{asset_path('fd-slider.js')}"
-  ],
-  callback: function(id, testResult) {
-    // If the slider file has loaded then fire the onDomReady event
-    if("fdSlider" in window && typeof (fdSlider.onDomReady) != "undefined") {
-      try { fdSlider.onDomReady(); } catch(err) {};
-    };
-  }
+if (!Modernizr.inputtypes.range) {
+  jQuery.getScript("#{asset_path('fd-slider.js')}")
+    .done(function(){
+        console.log('js loaded');
+    })
+    .fail(function(){
+        console.log('js not loaded');
+    });
+  $('<link/>', {
+      rel: 'stylesheet',
+      type: 'text/css',
+      href: "#{asset_path('fd-slider.css')}"
+   }).appendTo('head');
 }
-]);
