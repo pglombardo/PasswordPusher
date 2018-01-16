40bafdce051b9a55dcd2e15b038b3e80bc712b5a
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-01-16 14:45:40 +0100

    csp about, faq fix

--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -8,7 +8,7 @@ SecureHeaders::Configuration.default do |config|
     img_src: %w('self'),
     connect_src: %w('self'), # Ajax may connect only to the same origin
     #This is dirty. But this is a modernizr issue
-    style_src: %w('self' https://fonts.googleapis.com/ 'sha256-CwE3Bg0VYQOIdNAkbB/Btdkhul49qZuwgNCMPgNY5zw=' 'sha256-MZKTI0Eg1N13tshpFaVW65co/LeICXq4hyVx6GWVlK0=' 'sha256-LpfmXS+4ZtL2uPRZgkoR29Ghbxcfime/CsD/4w5VujE=' 'sha256-YJO/M9OgDKEBRKGqp4Zd07dzlagbB+qmKgThG52u/Mk='), # styles only allowed in external files from the same origin and in style attributes (for now)
+    style_src: %w('self' https://fonts.googleapis.com/ ), 
     font_src: %w('self' https://fonts.googleapis.com/ https://fonts.gstatic.com/)
    # report_uri: ["/csp_report?report_only=#{Rails.env.production?}“] # violation reports will be sent here
   }
