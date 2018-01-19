01a6baf91098d2e647db7dc0dad3fe1390fc83c8
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-01-19 18:22:40 +0100

    Fix blur

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,5 +51,5 @@ function setCopied() {
 	$('#clip_tip').text('copied!');
 }
 
-$('spoiler, .spoiler').spoilerAlert({max: 13, partial: 10})
+$('spoiler, .spoiler').spoilerAlert({max: 12, partial: 9})
 
