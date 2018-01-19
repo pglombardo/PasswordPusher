40b017a9915a19310c3361f7c1f26e6764344ef9
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-01-19 18:36:07 +0100

    Copy text

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -6,6 +6,14 @@ document.getElementById("url").addEventListener("focus",function(){
   document.getElementById("url").addEventListener("click",function(){
     $(this).select();
   });
+
+  document.getElementById("copyPass").addEventListener("click",function(){
+    var copyText = document.getElementById("payload_spoiler").textContent;
+    copyText.select();
+    document.execCommand("Copy");
+    alert("Password was copied to clipboard!");
+    return false;
+  });
   
 (function(){
     new Clipboard('#copyButton');
