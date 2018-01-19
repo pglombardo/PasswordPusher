b0fd70c7181674d265633dc986bde398c59ac7b0
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-01-19 14:46:15 +0100

    remove production from secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
-
+  config.read_encrypted_secrets = tru
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
@@ -80,7 +80,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
  
-  config.read_encrypted_secrets = true
+  e
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
