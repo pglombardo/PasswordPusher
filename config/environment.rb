c77da670b6d46d9c0feec1cd22cfdc4cf64632aa
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-01-19 14:17:21 +0100

    Fixed Enviroment Variables

--- config/environment.rb
+++ config/environment.rb
@@ -4,18 +4,18 @@ require_relative 'application'
 PAYLOAD_INITIAL_TEXT = ENV.fetch('PAYLOAD_INITIAL_TEXT', 'Enter the Password to be Shared')
 
 # If deploying PasswordPusher yourself, you should change these CRYPT values.
-CRYPT_KEY = ENV.fetch('CRYPT_KEY', '}s-#2R0^/+2wEXc47\$9Eb')
-CRYPT_SALT = ENV.fetch('CRYPT_SALT', ',2_%4?[+:3774>f')
+CRYPT_KEY = ENV.fetch('CRYPT_KEY', Rails.secrets.CRYPT_KEY)
+CRYPT_SALT = ENV.fetch('CRYPT_SALT', Rails.secrets.CRYPT_SALT)
 
 # Controls the "Expire After Days" form settings in Password#new
-EXPIRE_AFTER_TIME_DEFAULT = Integer(ENV.fetch('EXPIRE_AFTER_TIME_DEFAULT', 7))
+EXPIRE_AFTER_TIME_DEFAULT = Integer(ENV.fetch('EXPIRE_AFTER_TIME_DEFAULT', 1))
 EXPIRE_AFTER_TIME_MIN = Integer(ENV.fetch('EXPIRE_AFTER_TIME_MIN', 1))
-EXPIRE_AFTER_TIME_MAX = Integer(ENV.fetch('EXPIRE_AFTER_TIME_MAX', 90))
+EXPIRE_AFTER_TIME_MAX = Integer(ENV.fetch('EXPIRE_AFTER_TIME_MAX', 28))
 
 # Controls the "Expire After Views" form settings in Password#new
-EXPIRE_AFTER_VIEWS_DEFAULT = Integer(ENV.fetch('EXPIRE_AFTER_VIEWS_DEFAULT', 5))
+EXPIRE_AFTER_VIEWS_DEFAULT = Integer(ENV.fetch('EXPIRE_AFTER_VIEWS_DEFAULT', 1))
 EXPIRE_AFTER_VIEWS_MIN = Integer(ENV.fetch('EXPIRE_AFTER_VIEWS_MIN', 1))
-EXPIRE_AFTER_VIEWS_MAX = Integer(ENV.fetch('EXPIRE_AFTER_VIEWS_MAX', 100))
+EXPIRE_AFTER_VIEWS_MAX = Integer(ENV.fetch('EXPIRE_AFTER_VIEWS_MAX', 10))
 
 # DELETABLE_BY_VIEWER_PASSWORDS
 # Can passwords be deleted by viewers?
