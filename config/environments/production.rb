8ac0bcac247ede5eb48032a0dd171058ddfa9f80
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-01-19 15:24:04 +0100

    Load order fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
+  #Read these now. Not nice, but nothing else is working
+  CRYPT_KEY = ENV.fetch('CRYPT_KEY', Rails.application.secrets.CRYPT_KEY)
+  CRYPT_SALT = ENV.fetch('CRYPT_SALT', Rails.application.secrets.CRYPT_SALT)
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
