8f64a751ff4d95ed69e81f2c4d85913b3ce53b30
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-01-19 16:53:10 +0100

    Production fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
-  config.read_encrypted_secrets = true
+
   #Read these now. Not nice, but nothing else is working
 
   #This is currently the only way
