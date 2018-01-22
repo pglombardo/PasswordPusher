4a79c54b8a31267fc55bca814ed1d427611ae57c
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-01-22 12:05:55 +0100

    error fix

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,12 +1,13 @@
 //CSP-Fix
-document.getElementById("url").addEventListener("focus",function(){
-    $(this).focus(); $(this).select();
-  });
+if (document.getElementById("url") != null) {
+  document.getElementById("url").addEventListener("focus",function(){
+     $(this).focus(); $(this).select();
+   });
 
   document.getElementById("url").addEventListener("click",function(){
     $(this).select();
   });
-
+}
 (function(){
    if (document.getElementById("copyButton") != null) {
     new Clipboard('#copyButton');
