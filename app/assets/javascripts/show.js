b0959822e5217edcdbc0940b0c2b74987e03d94c
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-01-22 14:32:46 +0100

    semicolons

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -22,12 +22,12 @@ if (document.getElementById("url") != null) {
     new Clipboard('#payload_div')
     p_div.addEventListener("click",function(){
       if ($spoiler.data('spoiler-state') == 'revealed') {
-        p_div.dispatchEvent(new Event('switchBlur'))
+        p_div.dispatchEvent(new Event('switchBlur'));
       } else {
         if (document.queryCommandSupported("copy")){
-          alert("Password will be saved to your Clipboard!")
+          alert("Password will be saved to your Clipboard!");
         } else {
-          alert("After closing this press CTRL+v to copy the Password to your Clipboard!")
+          alert("After closing this press CTRL+v to copy the Password to your Clipboard!");
         }
     }
     });
