5507e7301e1aeba5c6bbbbc30e550e72b4f9dd39
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-01-22 13:03:47 +0100

    New reveal

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,4 +1,19 @@
 //CSP-Fix
+
+function unblur(spoiler) {
+  if (spoiler.data('spoiler-state') == 'shrouded') {
+    spoiler.data('spoiler-state', 'revealed')
+      .attr('title', '')
+      .css('cursor', 'auto')
+    performBlur(0, -1)
+  } else {
+    spoiler.data('spoiler-state', 'shrouded')
+      .attr('title', hintText)
+      .css('cursor', 'pointer')
+    performBlur(partialBlur, 1)
+  }
+}
+
 if (document.getElementById("url") != null) {
   document.getElementById("url").addEventListener("focus",function(){
      $(this).focus(); $(this).select();
@@ -13,7 +28,36 @@ if (document.getElementById("url") != null) {
     new Clipboard('#copyButton');
    } 
 
-   if (document.getElementById("copyLink") != null) {
-    new Clipboard('#copyLink');
-   } 
+   if (p_div = document.getElementById("payload_div") != null) {
+    new Clipboard('#payload_div');
+    p_div.addEventListener("click",function(){
+      if (p_div.data('spoiler-state') == 'revealed') {
+        unblur(p_div);
+      } else {
+        if (document.queryCommandSupported("copy")){
+          alert("Password has been saved to your Clipboard!")
+        } else {
+          alert("Press CTRL+v to copy the Password to your Clipboard!")
+        }
+    }
+    });
+
+    if (cLink = document.getElementById("copyLink") != null){
+      c_link.addEventListener("click", function(){
+        unblur(p_div);
+        setTimeout(function(){
+          if (p_div.data('spoiler-state') == 'revealed') {
+            unblur(p_div);
+          }
+        },10000)
+      })
+    }
+
+   }
+
+
+   
+   
+   
 })();
+
--- app/assets/javascripts/sitewide/spoiler.js
+++ app/assets/javascripts/sitewide/spoiler.js
@@ -81,19 +81,6 @@
       $spoiler.on('mouseout', function(e) {
         if ($spoiler.data('spoiler-state') == 'shrouded') performBlur(maxBlur, 1)
       })
-      $spoiler.on('click', function(e) {
-        if ($spoiler.data('spoiler-state') == 'shrouded') {
-          $spoiler.data('spoiler-state', 'revealed')
-            .attr('title', '')
-            .css('cursor', 'auto')
-          performBlur(0, -1)
-        } else {
-          $spoiler.data('spoiler-state', 'shrouded')
-            .attr('title', hintText)
-            .css('cursor', 'pointer')
-          performBlur(partialBlur, 1)
-        }
-      })
     })
 
   };
