6060136bb23b9a42447a5ef75a2af0ca2778fe81
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-02-13 16:52:57 +0100

    fix ssl

--- config/initializers/SecHead.rb
+++ config/initializers/SecHead.rb
@@ -1,8 +1,6 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
-
     preserve_schemes: true, # default: false.
-
     default_src: %w('none'), # all allowed in the beginning
     script_src: %w('self'), # scripts only allowed in external files from the same origin
     img_src: %w('self'),
@@ -12,7 +10,8 @@ SecureHeaders::Configuration.default do |config|
     font_src: %w('self' https://fonts.googleapis.com/ https://fonts.gstatic.com/),
     form_action: %w('self'),
     base_uri: %w('self'),
-    frame_ancestors: %w('none')
+    frame_ancestors: %w('none'),
+    upgrade-insecure-requests
    # report_uri: ["/csp_report?report_only=#{Rails.env.production?}“] # violation reports will be sent here
   }
 
