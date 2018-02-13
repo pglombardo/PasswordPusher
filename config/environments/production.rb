5cc0c62c8944c86b99b447b13d7b0d8ebb21d943
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-02-13 16:52:57 +0100

    fix ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   #config.ssl_options = {redirect: false}
   #config.ssl_options += { hsts: { expires: 6.months } }
-  config.force_ssl = true
+  #config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
