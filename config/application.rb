edcc7db9b45f52b78b68c837431f9cb138021e77
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2018-03-13 10:51:32 +0100

    localized fonts

--- config/application.rb
+++ config/application.rb
@@ -28,9 +28,8 @@ module PasswordPusher
     end
 
      # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.4'
-    config.assets.initialize_on_precompile = false
-    config.assets.precompile += ['fd-slider.css', 'fd-slider.js', 'modernizr.js','passwords.js','show.js']
+
+    
 
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,9 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.initialize_on_precompile = false
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += ['fd-slider.css', 'fd-slider.js', 'modernizr.js','passwords.js','show.js']
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
