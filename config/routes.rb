09e9c86e9949984ca6f54d7cb6112af80fc146ad
Peter Giacomo Lombardo <pglombardo@gmail.com>
2018-10-02 13:27:20 +0200

    Add scope to Slack direct install redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 PasswordPusher::Application.routes.draw do
   resources :p, :controller => :passwords, :as => :passwords, :except => [ :index, :edit, :update ]
   resources :c, :controller => :commands, :as => :commands, :allow => [ :create ]
-  get '/slack_direct_install', to: redirect("https://slack.com/oauth/authorize?client_id=#{SLACK_CLIENT_ID}", status: 302)
+  get '/slack_direct_install', to: redirect("https://slack.com/oauth/authorize?client_id=#{SLACK_CLIENT_ID}&scope=commands", status: 302)
   root :to => 'passwords#new'
 end
