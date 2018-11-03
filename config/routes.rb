3786bdd6d9e8d6d7a9a9faf02e93c2bb9451dd1f
Julien Amoros <julien@aircall.io>
2018-11-03 17:36:16 +0100

    impr(Views): add controller to see views of a password

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ PasswordPusher::Application.routes.draw do
   devise_for :users
   scope :users do
     get 'pwds', controller: :users, action: :passwords
+    get 'views', controller: :views, action: :index
+    get 'views/:id', controller: :views, action: :show, as: 'show_view'
   end
   resources :p, :controller => :passwords, :as => :passwords, :except => [ :index, :edit, :update ]
   resources :c, :controller => :commands, :as => :commands, :allow => [ :create ]
