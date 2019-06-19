af167ff3de060034a8a0f12f70799473623ebb5d
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2019-06-19 16:13:53 +0200

    New Headers

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ Bundler.require(*Rails.groups)
 module PasswordPusher
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -23,9 +23,9 @@ module PasswordPusher
     config.filter_parameters += [:password]
     # Enable the asset pipeline
     config.assets.enabled = true
-    if Rails.env.production?
-      config.read_encrypted_secrets = true
-    end
+    config.action_dispatch.default_headers.store('Report-To', '{"group":"default","max_age":31536000,"endpoints":[{"url":"https://d44c6675f6f03f85482859e657572968.report-uri.com/a/t/g"}],"include_subdomains":true}')
+    config.action_dispatch.default_headers.store('NEL', '{"report_to":"default","max_age":31536000,"include_subdomains":true, "success_fraction": 1.0,"failure_fraction": 1.0}')
+   
 
      # Version of your assets, change this if you want to expire all your assets
 
--- config/initializers/SecHead.rb
+++ config/initializers/SecHead.rb
@@ -6,6 +6,7 @@ SecureHeaders::Configuration.default do |config|
         preserve_schemes: true, # default: false.
         default_src: %w('none'), # all allowed in the beginning
         script_src: %w('self'), # scripts only allowed in external files from the same origin
+
         img_src: %w('self'),
         connect_src: %w('self'), # Ajax may connect only to the same origin
         #This is dirty. But this is a modernizr issue
@@ -15,7 +16,7 @@ SecureHeaders::Configuration.default do |config|
         base_uri: %w('self'),
         frame_ancestors: %w('none'),
         upgrade_insecure_requests: true,
-        report_uri: %w(https://secpush.report-uri.com/r/d/csp/enforce/) # violation reports will be sent here
+        report_uri: %w(https://d44c6675f6f03f85482859e657572968.report-uri.com/r/t/csp/enforce; report-to default) # violation reports will be sent here
       }
     config.hsts = "max-age=#{1.year.to_i}; includeSubdomains; preload"
   else
@@ -32,7 +33,7 @@ SecureHeaders::Configuration.default do |config|
       base_uri: %w('self'),
       frame_ancestors: %w('none'),
       upgrade_insecure_requests: false,
-      report_uri: %w(https://secpush.report-uri.com/r/d/csp/enforce/) # violation reports will be sent here
+      report_uri: %w(https://d44c6675f6f03f85482859e657572968.report-uri.com/r/t/csp/enforce; report-to default) # violation reports will be sent here
     }
 
   end
