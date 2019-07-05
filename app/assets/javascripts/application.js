dbefb88c1433c009cb9151fd32b16719fedfecf8
Leonard Schönborn <schoenbornl@int.smarthouse.de>
2019-07-05 08:39:44 +0200

    jquery autoload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,7 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require_tree ./libs
+//= require jquery
 //= require jquery_ujs
+//= require_tree ./libs
 //= require_tree ./sh
