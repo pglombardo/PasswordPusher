469286f806cde7e51c2e3305e953e68f88718036
Felix Greune <Felix.Greune@adesso-service.com>
2019-07-12 16:26:23 +0200

    Passwordfield hide secret atomaticly after 10 seconds.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,3 +10,47 @@
 //= require bootstrap-sprockets
 //= require_tree ./libs
 //= require_tree ./sh
+
+var counterToggle = 0;
+
+// Hide the Password after 10 Seconds again
+function hidePassword(element, checkNumber)
+{
+	setTimeout(function(){
+		// If false, the button is clicked again. So a other function on the
+		// stack wait 10 seconds to hide the password
+		if(counterToggle == checkNumber)
+		{
+			var id = $(element).attr("toggle");
+			if($(id).attr("type")== "text")
+			{
+				$(id).attr("type", "password")
+			}
+			adjustTooltipText(element);
+		}
+	}, 10000);
+}
+
+function adjustTooltipText(element)
+{
+	var id = $(element).attr("toggle");
+	if($(id).attr("type")== "text")
+	{
+		$(element).attr("data-original-title", "Hide the secret");
+	}
+	else
+	{
+		$(element).attr("data-original-title", "Show the secret for 10 seconds");
+	}
+}
+
+$(document).ready(function() {
+	
+	// Events
+	$(".toggle-password").click(function(){
+		counterToggle++;
+		hidePassword(this, counterToggle);
+		adjustTooltipText(this);
+	});
+	
+});
