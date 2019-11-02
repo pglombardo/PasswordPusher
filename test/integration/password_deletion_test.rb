827b6dc14ff391d4501acd11974533732485b8e7
Peter Giacomo Lombardo <pglombardo@gmail.com>
2019-11-02 10:45:36 +0100

    Add deletion integration tests

---
+++ test/integration/password_deletion_test.rb
@@ -0,0 +1,47 @@
+require 'test_helper'
+
+class PasswordCreationTest < ActionDispatch::IntegrationTest
+  def test_password_deletion
+    get "/"
+    assert_response :success
+
+    post "/p", :password => { payload: "testpw" }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "p", "Your password is..."
+
+  end
+
+  def test_json_password_deletion
+    post "/p.json", :password => { payload: "testpw" }
+    assert_response :success
+
+    res = JSON.parse(@response.body)
+    assert res.key?("id")
+    assert res.key?("payload")
+    assert res.key?("url_token")
+    assert res.key?("expired")
+    assert_equal false, res["expired"]
+    assert res.key?("deleted")
+    assert_equal false, res["deleted"]
+    assert res.key?("deletable_by_viewer")
+    assert_equal false, res["deletable_by_viewer"]
+
+    # Get the new password via json e.g. /p/<url_token>.json
+    delete "/p/" + res["url_token"] + ".json"
+    assert_response :success
+
+    res = JSON.parse(@response.body)
+    assert res.key?("id")
+    assert res.key?("payload")
+    assert_nil res["payload"]
+    assert res.key?("url_token")
+    assert res.key?("expired")
+    assert_equal true, res["expired"]
+    assert res.key?("deleted")
+    assert_equal true, res["deleted"]
+    assert res.key?("deletable_by_viewer")
+    assert_equal false, res["deletable_by_viewer"]
+  end
+end
