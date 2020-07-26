b274cf9011f11ae8fc29c101206e84be3a90f8d0
Peter Giacomo Lombardo <nosis@rbx.run>
2020-07-26 10:53:04 +0200

    Rails app:update results

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-PasswordPusher::Application.routes.draw do
+Rails.application.routes.draw do
   resources :p, :controller => :passwords, :as => :passwords, :except => [ :index, :edit, :update ]
   resources :c, :controller => :commands, :as => :commands, :allow => [ :create ]
   get '/slack_direct_install', to: redirect("https://slack.com/oauth/authorize?client_id=#{SLACK_CLIENT_ID}&scope=commands", status: 302)
