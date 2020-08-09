fcd7115a994f81577d208d71a578c2447121e1af
Peter Giacomo Lombardo <nosis@rbx.run>
2020-08-09 16:02:44 +0200

    Framework: Finalize 5.2 defaults (#143)
    * Remove 5.2 upgrade crutch.  Now load 5.2 defaults
    
    * Update config load

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module PasswordPusher
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
