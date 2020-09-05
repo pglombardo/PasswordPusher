807aff0f8b6cf98ebbe13ab7da26e34bf22c6174
Peter Giacomo Lombardo <nosis@rbx.run>
2020-09-05 11:06:13 +0200

    Use :hybrid cookie serialization

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -2,5 +2,9 @@
 
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
-# Marshal has a known RCE vulnerability. Use :json instead.
-Rails.application.config.action_dispatch.cookies_serializer = :json
+#
+# Marshal has a potential RCE vulnerability.
+# If we switch directly to :json, app can 500 on cookie deserialization for old cookies
+# Use :hybrid for now and aim for :json eventually.
+# https://github.com/presidentbeef/brakeman/issues/1316
+Rails.application.config.action_dispatch.cookies_serializer = :hybrid
