1451628ce659ea566f8393451580f21bb601ae7c
Peter Giacomo Lombardo <nosis@rbx.run>
2020-10-27 20:39:17 +0100

    Logging: Set to info level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,10 +56,10 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
