778eea01905337e41f8752c0b19106fda1c3e864
Peter Giacomo Lombardo <pglombardo@hey.com>
2021-09-13 13:39:22 +0200

    User relation to passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable, :confirmable, :lockable
+
+  has_many :passwords
 end
