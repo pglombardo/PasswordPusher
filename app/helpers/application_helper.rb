0e689aa272e441071c9205edadda5ec10e9e8ac1
Peter Giacomo Lombardo <pglombardo@hey.com>
2021-09-18 19:33:19 +0200

    Release Touchups (#218)
    * Remove page title branding from password pages
    
    * Build _latest_ docker containers daily

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
+  # Set the HTML title for the page with a trailing site identifier.
   def title(content)
     content_for(:html_title) { "#{content} | Password Pusher" }
+  end
+
+  # Set the HTML title for the page _without_ the trailing site identifier.
+  # Used in Password#show/show_expired to hide Password Pusher branding
+  def plain_title(content)
+    content_for(:html_title) { content }
   end
 end
