42d0a90a815b86c35fe3310e4743e17fbd526ec5
Peter Giacomo Lombardo <pglombardo@hey.com>
2021-10-02 22:37:25 +0200

    Add Serbian locale

--- config/initializers/translation.rb
+++ config/initializers/translation.rb
@@ -1,5 +1,5 @@
 # Permitted locales available for the application
-I18n.available_locales = %i[en es it nl]
+I18n.available_locales = %i[en es it nl sr]
 
 # Set default locale to something other than :en
 I18n.default_locale = :en
