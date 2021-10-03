1244ea52e9993640d29dfb5e477920fa12c2f45d
Peter Giacomo Lombardo <pglombardo@hey.com>
2021-10-03 14:15:57 +0200

    Eager loading; Fix DateTime localization

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,8 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def active
-    @active_pushes = Password.where(user_id: current_user.id, expired: false)
+    @active_pushes = Password.includes(:views)
+                             .where(user_id: current_user.id, expired: false)
                              .paginate(page: params[:page], per_page: 30)
                              .order(created_at: :desc)
   end
