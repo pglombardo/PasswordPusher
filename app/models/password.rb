7ec307ff25a23826b0c2165f72be0f19ee43f56b
Peter Giacomo Lombardo <pglombardo@hey.com>
2021-10-04 19:55:02 +0200

    Rake data deletion task; 404 old records

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,5 +1,4 @@
 class Password < ApplicationRecord
-  # attr_accessible :payload, :expire_after_days, :expire_after_views, :deletable_by_viewer
   has_many :views, dependent: :destroy
 
   def to_param
