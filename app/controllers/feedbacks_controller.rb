28a0df1103ca87ef93ad229a390f07ad7ef8bf0b
Peter Giacomo Lombardo <pglombardo@hey.com>
2021-10-05 19:47:44 +0200

    Check for and reject spam requests (#234)

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -5,12 +5,18 @@ class FeedbacksController < ApplicationController
 
   def create
     @feedback = Feedback.new(params[:feedback])
-    @feedback.request = request
-    if @feedback.deliver
-      flash.now[:success] = _('Feedback sent!')
-    else
-      flash.now[:error] = _('Could not send feedback.  Try again?')
+
+    if @feedback.spam?
+      flash.now[:alert] = _('Our apologies but you failed the spam check.  You could try contacting us on Github instead.')
       render :new
+    else
+      @feedback.request = request
+      if @feedback.deliver
+        flash.now[:notice] = _('Feedback sent!')
+      else
+        flash[:alert] = _('Could not send feedback.  Try again?')
+        render :new
+      end
     end
   end
 end
