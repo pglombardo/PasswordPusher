099cc226412a40632486c249b90ad47c7759ba45
Peter Giacomo Lombardo <pglombardo@hey.com>
2021-11-18 14:52:35 +0100

    Add login support to ephemeral version for demo & testing

--- config/environments/private.rb
+++ config/environments/private.rb
@@ -9,4 +9,23 @@ PasswordPusher::Application.configure do
   config.active_support.deprecation = :notify
   config.eager_load = false
   config.force_ssl = ENV.key?('FORCE_SSL') ? true : false
+
+  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = Settings.mail.raise_delivery_errors
+
+  config.action_mailer.default_url_options = {
+    host: Settings.host_domain,
+    protocol: Settings.host_protocol
+  }
+
+  config.action_mailer.smtp_settings = {
+    address: Settings.mail.smtp_address,
+    port: Settings.mail.smtp_port,
+    user_name: Settings.mail.smtp_user_name,
+    password: Settings.mail.smtp_password,
+    authentication: Settings.mail.smtp_authentication,
+    enable_starttls_auto: Settings.mail.smtp_starttls,
+    open_timeout: Settings.mail.smtp_open_timeout,
+    read_timeout: Settings.mail.smtp_read_timeout
+  }
 end
