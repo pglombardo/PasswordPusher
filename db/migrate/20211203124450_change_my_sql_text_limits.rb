f3be8fbafe05b68558e9375015ff9fcb0a424bfd
Peter Giacomo Lombardo <pglombardo@hey.com>
2021-12-03 13:48:34 +0100

    MySQL: Set a limit of about ~16MB so MEDIUMTEXT is used

---
+++ db/migrate/20211203124450_change_my_sql_text_limits.rb
@@ -0,0 +1,6 @@
+class ChangeMySqlTextLimits < ActiveRecord::Migration[6.1]
+  def change
+    # MySQL defaults to the smallest test field type.  Set a limit of about ~16MB so MEDIUMTEXT is used
+    change_column :passwords, :payload_ciphertext, :text, limit: 16.megabytes - 1
+  end
+end
