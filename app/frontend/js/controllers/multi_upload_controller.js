474749074f17df8804ba2a954d5045d24e8e9932
Peter Giacomo Lombardo <pglombardo@hey.com>
2021-12-12 10:47:42 +0100

    Remove debug stuff

--- app/frontend/js/controllers/multi_upload_controller.js
+++ app/frontend/js/controllers/multi_upload_controller.js
@@ -10,16 +10,10 @@ function formatBytes(bytes, decimals = 2) {
 }
 
 export default class extends Controller {
-  // filesTarget is going to contain the list of input elements we've added
-  // files to - in other words, these are the input elements that we're going
-  // to submit.
+  // Target contains the selected file list
   static targets = ["files"]
 
   addFile(event) {
-
-    window.blah = event
-
-    // Grab some references for later
     const originalInput = event.target
     const originalParent = originalInput.parentNode
 
@@ -35,36 +29,22 @@ export default class extends Controller {
       trashIcon.classList = 'bi bi-trash me-2'
       
       var trashLink = document.createElement("a")
-      // trashLink.href = '#'
       trashLink.setAttribute('data-action', 'multi-upload#removeFile')
       trashLink.appendChild(trashIcon)
       selectedFile.appendChild(trashLink)
     
-      // Create label (the visible part of the new input element) with the name of
-      // the selected file.
       var textElement = document.createTextNode(fileName);
       selectedFile.appendChild(textElement);
     
-      // Add the selected file to the list of selected files
       this.filesTarget.append(selectedFile)
     }
     
-    // Create a new input field to use going forward
     const newInput = originalInput.cloneNode()
-  
-    // Clear the filelist - some browsers maintain the filelist when cloning,
-    // others don't
     newInput.value = ""
-  
-    // Add it to the DOM where the original input was
     originalParent.append(newInput)
   }
   
   removeFile(event) {
-
-    window.fdsa = event
-
-    // Grab some references for later
     const listItem = event.target.parentNode.parentNode
     const list = listItem.parentNode
 
