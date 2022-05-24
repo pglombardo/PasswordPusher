5e8cdca32552dee1328de2b69f286020ad700bb5
Peter Giacomo Lombardo <pglombardo@hey.com>
2022-05-24 19:04:13 +0200

    Update tests to follow

--- test/controllers/password_controller_test.rb
+++ test/controllers/password_controller_test.rb
@@ -4,6 +4,6 @@ class PasswordControllerTest < ActionDispatch::IntegrationTest
   test 'New push form is available anonymous' do
     get new_password_path
     assert_response :success
-    assert response.body.include?('Tip: Only enter the password into the box')
+    assert response.body.include?('Tip: Only enter a password into the box')
   end
 end
