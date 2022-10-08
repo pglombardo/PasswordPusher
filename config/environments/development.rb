da44acf46e06050a4d05341aaee56115d2f7f1e4
Peter Giacomo Lombardo <pglombardo@hey.com>
2022-10-08 16:30:20 +0200

    Update default log levels

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
     config.action_mailer.smtp_settings[:enable_starttls] = Settings.mail.smtp_enable_starttls
   end
 
-  config.log_level = ENV.key?('LOG_LEVEL') ? ENV['LOG_LEVEL'].to_sym : :warn
+  config.log_level = ENV.key?('LOG_LEVEL') ? ENV['LOG_LEVEL'].to_sym : :debug
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
