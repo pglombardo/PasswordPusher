bffb2a0d6fd55f7a9cee537c8f2bfec259ca13ac
Peter Giacomo Lombardo <pglombardo@hey.com>
2022-11-13 16:42:25 +0100

    update entrypoints for postgres and mysql containers

--- containers/docker/pwpush-postgres/entrypoint.sh
+++ containers/docker/pwpush-postgres/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
-RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production bundle exec puma -C config/puma.rb
+export RAILS_ENV=production
+
+echo "Password Pusher: migrating database to latest..."
+bundle exec rake db:migrate
+echo "Password Pusher: precompiling assets..."
+bundle exec rails webpacker:compile
+echo "Password Pusher: starting puma webserver..."
+bundle exec puma -C config/puma.rb
 
 exec "$@"
