263f8aa410f0aab979bcee2ef080ae897b6be317
Peter Giacomo Lombardo <pglombardo@hey.com>
2022-11-13 16:36:30 +0100

    pwpush-ephemeral: migrate and compile on container boot

---
+++ containers/docker/pwpush-ephemeral/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+export RAILS_ENV=private
+
+echo "Password Pusher: migrating database to latest..."
+bundle exec rake db:migrate
+echo "Password Pusher: precompiling assets..."
+bundle exec rails webpacker:compile
+echo "Password Pusher: starting puma webserver..."
+bundle exec puma -C config/puma.rb -e private
+
+exec "$@"
