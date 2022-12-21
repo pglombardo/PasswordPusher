864046c94838a85c5ce4ed7e26ca4e5a5ebf2538
Peter Giacomo Lombardo <pglombardo@hey.com>
2022-12-21 14:58:56 +0100

    Add ability to run app in subfolder (#306)
    * Add ability to run app in subfolder
    
    * update defaults
    
    * Add to Configuration documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,49 +1,60 @@
 Rails.application.routes.draw do
-  if ENV.key?('PWPUSH_COM')
-    match '(*any)', to: redirect(subdomain: ''), via: :all, constraints: {subdomain: 'www'}
-  end
-
-  apipie
-  localized do
-    devise_for :users, skip: :registrations, controllers: {
-      sessions: 'users/sessions',
-      passwords: 'users/passwords',
-      unlocks: 'users/unlocks',
-      confirmations: 'users/confirmations'
-    }
-
-    if Settings.disable_signups
-      allowed_reg_routes = %i[edit update]
-    else
-      allowed_reg_routes = %i[new create edit update]
+  routes_config = Proc.new {
+    if ENV.key?('PWPUSH_COM')
+      match '(*any)', to: redirect(subdomain: ''), via: :all, constraints: {subdomain: 'www'}
     end
 
-    devise_scope :user do
-      resource  :registration,
-                only: allowed_reg_routes,
-                path: 'users',
-                path_names: { new: 'sign_up' },
-                controller: 'users/registrations',
-                as: :user_registration do
-                  get :cancel
-                  get :token
-                  delete :token, action: :regen_token
-                end
+    apipie
+    localized do
+      devise_for :users, skip: :registrations, controllers: {
+        sessions: 'users/sessions',
+        passwords: 'users/passwords',
+        unlocks: 'users/unlocks',
+        confirmations: 'users/confirmations'
+      }
+
+      if Settings.disable_signups
+        allowed_reg_routes = %i[edit update]
+      else
+        allowed_reg_routes = %i[new create edit update]
+      end
+
+      devise_scope :user do
+        resource  :registration,
+                  only: allowed_reg_routes,
+                  path: 'users',
+                  path_names: { new: 'sign_up' },
+                  controller: 'users/registrations',
+                  as: :user_registration do
+                    get :cancel
+                    get :token
+                    delete :token, action: :regen_token
+                  end
+      end
+
+      get '/d/active' => 'dashboard#active', as: :dashboard_active
+      get '/d/expired' => 'dashboard#expired', as: :dashboard_expired
+
+      resources :p, controller: :passwords, as: :passwords, except: %i[index edit update] do
+        get 'preview', on: :member
+        get 'r', on: :member, as: 'preliminary', action: 'preliminary'
+        get 'audit', on: :member
+      end
+
+      resources :c, controller: :commands, as: :commands, allow: %i[create]
+      # get '/slack_direct_install', to: redirect("https://slack.com/oauth/authorize?client_id=#{SLACK_CLIENT_ID}&scope=commands", status: 302)
+      get '/pages/*id' => 'pages#show', as: :page, format: false
+      resources :feedbacks, only: %i[new create]
+      root to: 'passwords#new'
     end
+  }
 
-    get '/d/active' => 'dashboard#active', as: :dashboard_active
-    get '/d/expired' => 'dashboard#expired', as: :dashboard_expired
-
-    resources :p, controller: :passwords, as: :passwords, except: %i[index edit update] do
-      get 'preview', on: :member
-      get 'r', on: :member, as: 'preliminary', action: 'preliminary'
-      get 'audit', on: :member
+  # This allows for running the application in a subfolder.  See config/settings.yml (relative_root)
+  if Settings.relative_root
+    scope(:path => Settings.relative_root) do
+      routes_config.call
     end
-
-    resources :c, controller: :commands, as: :commands, allow: %i[create]
-    # get '/slack_direct_install', to: redirect("https://slack.com/oauth/authorize?client_id=#{SLACK_CLIENT_ID}&scope=commands", status: 302)
-    get '/pages/*id' => 'pages#show', as: :page, format: false
-    resources :feedbacks, only: %i[new create]
-    root to: 'passwords#new'
+  else
+    routes_config.call
   end
 end
