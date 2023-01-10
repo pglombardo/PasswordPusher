834dcdb84bbbd2548a800cef77c706c52be13210
Peter Giacomo Lombardo <pglombardo@hey.com>
2023-01-10 15:57:50 +0100

    Ensure all links don't reset the locale

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -102,7 +102,7 @@ class PasswordsController < ApplicationController
     password_param = params.fetch(:password, {})
     if !password_param.respond_to?(:fetch)
       respond_to do |format|
-        format.html { redirect_to root_path, status: :bad_request, notice: 'Bad Request' }
+        format.html { redirect_to root_path(locale: locale.to_s), status: :bad_request, notice: 'Bad Request' }
         format.json { render json: { "error": "No password, text or files provided." }, status: :bad_request }
       end
       return
@@ -114,7 +114,7 @@ class PasswordsController < ApplicationController
     payload_param = password_param.fetch(:payload, '')
     unless payload_param.is_a?(String) && payload_param.length.between?(1, 1.megabyte)
       respond_to do |format|
-        format.html { redirect_to root_path, status: :bad_request, notice: 'Bad Request' }
+        format.html { redirect_to root_path(locale: locale.to_s), status: :bad_request, notice: 'Bad Request' }
         format.json { render json: { "error": "Payload length must be between 1 and 1_048_576." }, status: :bad_request }
       end
       return
