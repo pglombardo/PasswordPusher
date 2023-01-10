854736279aabd01b85f5f61885f505789ad22de9
Peter Giacomo Lombardo <pglombardo@hey.com>
2023-01-10 12:01:34 +0100

    Page tests

---
+++ test/integration/pages/generate_key_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class GenerateKeyPageTest < ActionDispatch::IntegrationTest
+  def test_generate_key_page_renders_ok
+    get page_path('generate_key')
+    assert_response :success
+  end
+end
