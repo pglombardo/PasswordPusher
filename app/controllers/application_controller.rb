88f8fa8f6bf313a08e6b51c1dea9c7566da143e7
Peter Giacomo Lombardo <pglombardo@hey.com>
2023-02-16 22:16:31 +0100

    Default Locale: Use custom handler to force default fallback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
-  around_action :set_locale_from_url
+  around_action :custom_set_locale_from_url
 
   add_flash_types :info, :error, :success, :warning
+
+  def custom_set_locale_from_url
+    locale_from_url = RouteTranslator.locale_from_params(params) || RouteTranslator::Host.locale_from_host(request.host) || I18n.default_locale
+    if locale_from_url
+      old_locale  = I18n.locale
+      I18n.locale = locale_from_url
+    end
+
+    yield
+  ensure
+    I18n.locale = old_locale if locale_from_url
+  end
 
   def not_found
     raise ActionController::RoutingError.new(_('Not Found'))
