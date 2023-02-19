33442fb7fa269d5df0390166baea48d20a053c46
Peter Giacomo Lombardo <pglombardo@hey.com>
2023-02-19 12:52:19 +0100

    Settings: Add option to disable Password Push blurring

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -59,6 +59,9 @@ class PasswordsController < ApplicationController
     log_view(@push)
     expires_now
 
+    # Optionally blur the password
+    @use_blur = Settings.pw.enable_blur ? 'spoiler' : ''
+
     respond_to do |format|
       format.html { render layout: 'bare' }
       format.json { render json: @push.to_json(payload: true) }
