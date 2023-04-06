e6d91663c0855480b6faa9f519df74f99b2e9bef
Peter Giacomo Lombardo <pglombardo@hey.com>
2023-04-06 10:38:59 +0200

    Simplify code: Remove hard-coded strings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,7 @@
 module ApplicationHelper
   # Set the HTML title for the page with a trailing site identifier.
   def title(content)
-    if Settings.brand.title
-      return content_for(:html_title) { "#{content} | #{Settings.brand.title}" }
-    else
-      return content_for(:html_title) { "#{content} | #{_('Password Pusher')}" }
-    end
+    content_for(:html_title) { "#{content} | #{Settings.brand.title}" }
   end
 
   # Set the HTML title for the page _without_ the trailing site identifier.
