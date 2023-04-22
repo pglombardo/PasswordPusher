45c8f530fbf2a399896135eef7c5a67367361aa6
Peter Giacomo Lombardo <pglombardo@hey.com>
2023-04-22 15:42:49 +0200

    Exception based db migrations

--- db/migrate/20230406094931_add_passphrase_to_models.rb
+++ db/migrate/20230406094931_add_passphrase_to_models.rb
@@ -1,27 +1,18 @@
 class AddPassphraseToModels < ActiveRecord::Migration[7.0]
   def change
-    # This migration breaks on MySQL Server so we need to check what we are using
+    # The column defaults on this migration breaks on MySQL Server.
     # See: https://github.com/pglombardo/PasswordPusher/issues/1002
-    #
-    # Between this migration and the next, we will remove the default values entirely
-    # and both groups of MySQL servers (MySQL server & MariaDB) will be happy.
-
-    using_mysql_server = false
-    if ActiveRecord::Base.connection.is_a?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
-      results = ActiveRecord::Base.connection.execute('show variables like "version_comment%";')
-      using_mysql_server = (results.first[1] =~ /mariadb/i) == nil
-    end
-
-    if using_mysql_server
-      # MySQL Server (not MariaDB)
-      add_column :passwords,   :passphrase_ciphertext, :text, :limit => 2048
-      add_column :file_pushes, :passphrase_ciphertext, :text, :limit => 2048
-      add_column :urls,        :passphrase_ciphertext, :text, :limit => 2048
-    else
-      # Every database except MySQL Server
+    begin
       add_column :passwords,   :passphrase_ciphertext, :text, :null => false, :default => '', :limit => 2048
       add_column :file_pushes, :passphrase_ciphertext, :text, :null => false, :default => '', :limit => 2048
       add_column :urls,        :passphrase_ciphertext, :text, :null => false, :default => '', :limit => 2048
+    rescue ActiveRecord::StatementInvalid => e
+      # MySQL Server doesn't support column defaults for TEXT columns
+      Rails.logger.warn("Failed to add passphrase_ciphertext columns with defaults")
+      Rails.logger.warn("This is expected if you are using MySQL")
+      add_column :passwords,   :passphrase_ciphertext, :text, :limit => 2048
+      add_column :file_pushes, :passphrase_ciphertext, :text, :limit => 2048
+      add_column :urls,        :passphrase_ciphertext, :text, :limit => 2048
     end
   end
 end
--- db/migrate/20230420175410_fix_passphrase_default.rb
+++ db/migrate/20230420175410_fix_passphrase_default.rb
@@ -1,29 +1,32 @@
 class FixPassphraseDefault < ActiveRecord::Migration[7.0]
   def change
-    # The previous migration breaks on MySQL Server.  See previous migration for an explanation.
-
-    using_mysql_server = false
-    if ActiveRecord::Base.connection.is_a?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
-      results = ActiveRecord::Base.connection.execute('show variables like "version_comment%";')
-      using_mysql_server = (results.first[1] =~ /mariadb/i) == nil
-    end
-
-    if !using_mysql_server
-      # Every database except MySQL Server
-
+    begin
       # Add ability to set null values
       change_column_null(:passwords,   :passphrase_ciphertext, true)
       change_column_null(:file_pushes, :passphrase_ciphertext, true)
       change_column_null(:urls,        :passphrase_ciphertext, true)
+    rescue ActiveRecord::StatementInvalid => e
+      # This will fail because of the problems in the previous migration re: MySQL
+      # Failure here is okay
+      Rails.logger.warn("Failed to set null values for passphrase_ciphertext: #{e}")
+      Rails.logger.warn("This is expected if you are using MySQL")
+    end
 
+    begin
       # Remove default values
       change_column_default(:passwords,   :passphrase_ciphertext, nil)
       change_column_default(:file_pushes, :passphrase_ciphertext, nil)
       change_column_default(:urls,        :passphrase_ciphertext, nil)
+    rescue ActiveRecord::StatementInvalid => e
+      # This will fail because of the problems in the previous migration re: MySQL
+      # Failure here is okay
+      Rails.logger.warn("Failed to remove default values for passphrase_ciphertext: #{e}")
+      Rails.logger.warn("This is expected if you are using MySQL")
+    end
 
-      # Change all empty string passphrases to nulls
-      Password.where(passphrase_ciphertext: '').update_all(passphrase_ciphertext: nil)
-      FilePush.where(passphrase_ciphertext: '').update_all(passphrase_ciphertext: nil)
-      Url.where(passphrase_ciphertext: '').update_all(passphrase_ciphertext: nil)
+    # Change all empty string passphrases to nulls
+    Password.where(passphrase_ciphertext: '').update_all(passphrase_ciphertext: nil)
+    FilePush.where(passphrase_ciphertext: '').update_all(passphrase_ciphertext: nil)
+    Url.where(passphrase_ciphertext: '').update_all(passphrase_ciphertext: nil)
   end
 end
