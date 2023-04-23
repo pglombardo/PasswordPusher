2a09e990cf0845ae69df49578fc22d429113a625
Peter Giacomo Lombardo <pglombardo@hey.com>
2023-04-23 19:14:51 +0200

    Passphrase Lockdown: null handling

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -52,7 +52,7 @@ class UrlsController < ApplicationController
     end
 
     # Passphrase handling
-    if !@push.passphrase.blank?
+    if !@push.passphrase.nil? && !@push.passphrase.blank?
       # Construct the passphrase cookie name
       name = @push.url_token + '-' + 'r'
 
