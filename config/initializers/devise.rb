94abe883f36f0ebc80ce0cef98f77bcf82d97fff
Peter Giacomo Lombardo <pglombardo@hey.com>
2023-07-26 16:30:29 +0200

    Add option to limit signup to specific domains

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -183,10 +183,8 @@ Devise.setup do |config|
   # Range for password length.
   config.password_length = 6..128
 
-  # Email regex used to validate email formats. It simply asserts that
-  # one (and only one) @ exists in the given string. This is mainly
-  # to give user feedback and not to assert the e-mail validity.
-  config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+  # Email regex used to validate email formats.
+  config.email_regexp = Regexp.new(Settings.signup_email_regexp)
 
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
