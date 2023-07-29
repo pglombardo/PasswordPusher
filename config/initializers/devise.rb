e29dce90136d8011a130fdac47cc7359bbea343c
Peter Giacomo Lombardo <pglombardo@hey.com>
2023-07-30 00:14:05 +0200

    Add option to limit signups to those with specific email domains (#1283)
    * Add option to limit signup to specific domains
    
    * Fix regexp strings
    
    * Require users to confirm email before login
    
    * Add docs for signup_email_regexp

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -146,7 +146,7 @@ Devise.setup do |config|
   # without confirming their account.
   # Default is 0.days, meaning the user cannot access the website without
   # confirming their account.
-  config.allow_unconfirmed_access_for = 2.days
+  config.allow_unconfirmed_access_for = 0.days
 
   # A period that the user is allowed to confirm their account before their
   # token becomes invalid. For example, if set to 3.days, the user can confirm
@@ -183,10 +183,8 @@ Devise.setup do |config|
   # Range for password length.
   config.password_length = 6..128
 
-  # Email regex used to validate email formats. It simply asserts that
-  # one (and only one) @ exists in the given string. This is mainly
-  # to give user feedback and not to assert the e-mail validity.
-  config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+  # Email regex used to validate email formats.
+  config.email_regexp = Regexp.new(Settings.signup_email_regexp)
 
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
