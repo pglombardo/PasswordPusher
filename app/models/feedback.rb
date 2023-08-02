1b8e9324ccb3a20c3be1f2dddb71b971f92c80bd
Peter Giacomo Lombardo <pglombardo@hey.com>
2023-08-02 12:06:47 +0200

    Cleanup

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -5,19 +5,18 @@ class Feedback < MailForm::Base
   attribute :name,      validate: true
   attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   validate: true
-  attribute :nickname,  captcha: true
-  attribute :control,   validate: /\A9\z/
+  attribute :control,   validate: /\A95\z/
+  attributes :nickname, captcha: true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
     headers = {
       to: Settings.feedback.email,
-      from: Settings.mail.mailer_sender
+      from: Settings.mail.mailer_sender,
+      subject: Settings.brand.title + ' Feedback',
+      'X-PWPUSH-URL' => request.url
     }
-
-    headers[:subject] = Settings.brand.title + ' Feedback'
-
     headers
   end
 end
