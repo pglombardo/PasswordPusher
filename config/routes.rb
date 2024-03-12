03f6268c87367f4d8bc0a5c77def411096602b9a
Peter Giacomo Lombardo <pglombardo@hey.com>
2024-03-12 22:30:07 +0100

    Disable Login Routes when Logins are Disabled

--- config/routes.rb
+++ config/routes.rb
@@ -5,35 +5,34 @@ Rails.application.routes.draw do
 
   localized do
     apipie
-    devise_for :users, skip: :registrations, controllers: {
-      sessions: "users/sessions",
-      passwords: "users/passwords",
-      unlocks: "users/unlocks",
-      confirmations: "users/confirmations"
-    }
 
-    allowed_reg_routes = if Settings.disable_signups
-      %i[edit update]
-    else
-      %i[new create edit update]
-    end
+    if Settings.enable_logins
+      devise_for :users, skip: :registrations, controllers: {
+        sessions: "users/sessions",
+        passwords: "users/passwords",
+        unlocks: "users/unlocks",
+        confirmations: "users/confirmations"
+      }
 
-    devise_scope :user do
-      resource :registration,
-        only: allowed_reg_routes,
-        path: "users",
-        path_names: {new: "sign_up"},
-        controller: "users/registrations",
-        as: :user_registration do
-        get :cancel
-        get :token
-        delete :token, action: :regen_token
+      allowed_reg_routes = if Settings.disable_signups
+        %i[edit update]
+      else
+        %i[new create edit update]
       end
-    end
 
-    # Dashboard controller has been removed.  Maintain this remapping for now.
-    get "/d/active", to: "passwords#active"
-    get "/d/expired", to: "passwords#expired"
+      devise_scope :user do
+        resource :registration,
+          only: allowed_reg_routes,
+          path: "users",
+          path_names: {new: "sign_up"},
+          controller: "users/registrations",
+          as: :user_registration do
+          get :cancel
+          get :token
+          delete :token, action: :regen_token
+        end
+      end
+    end # if Settings.enable_logins
 
     resources :p, controller: :passwords, as: :passwords, except: %i[index edit update] do
       get "preview", on: :member
