3d920be5cd189ef9b95a5cb0a847cd360d182e04
Peter Giacomo Lombardo <pglombardo@hey.com>
2024-04-09 18:37:10 +0200

    More checks and documentation

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,6 +1,8 @@
 class TestMailer < ApplicationMailer
   def send_test_email(email)
     puts "Configured from address: '#{Settings.mail.mailer_sender}'"
+    raise StandardError, "No host domain provided: host_domain" if Settings.host_domain.nil?
+    raise StandardError, "No host domain protocol provided: host_protocol" if Settings.host_protocol.nil?
     raise StandardError, "No SMTP host address provided: smtp_address" if Settings.mail.smtp_address.nil?
     raise StandardError, "No SMTP port provided: smtp_port" if Settings.mail.smtp_port.nil?
     raise StandardError, "No SMTP username provided: smtp_user_name" if Settings.mail.smtp_user_name.nil?
