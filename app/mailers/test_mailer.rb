2a69979432a49a34cc936fe6c9799b4e0c67505c
Peter Giacomo Lombardo <pglombardo@hey.com>
2024-04-09 14:20:34 +0200

    TestMailer: Add more checks

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,5 +1,11 @@
 class TestMailer < ApplicationMailer
   def send_test_email(email)
+    puts "Configured from address: '#{Settings.mail.mailer_sender}'"
+    raise StandardError, "No SMTP host address provided: smtp_address" if Settings.mail.smtp_address.nil?
+    raise StandardError, "No SMTP port provided: smtp_port" if Settings.mail.smtp_port.nil?
+    raise StandardError, "No SMTP username provided: smtp_user_name" if Settings.mail.smtp_user_name.nil?
+    raise StandardError, "No SMTP password provided: smtp_password" if Settings.mail.smtp_password.nil?
+
     mail(to: email,
       subject: "Test Email from Password Pusher",
       body: "⭐ If you are reading this, sending email works! ⭐")
