e578a7d64922d6d73cb1faee9c1ad0826e4cefe6
Peter Giacomo Lombardo <pglombardo@hey.com>
2024-04-14 13:55:30 +0200

    Set default sort for Views

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Password < ApplicationRecord
-  has_many :views, dependent: :destroy
+  has_many :views, -> { order(created_at: :asc) }, dependent: :destroy
   has_encrypted :payload, :note, :passphrase
 
   belongs_to :user, optional: true
