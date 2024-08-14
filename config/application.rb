436ea5303d8b116f52be5420b7864cd0f4b1c965
Peter Giacomo Lombardo <pglombardo@hey.com>
2024-08-14 12:05:02 +0200

    Application Framework Updates

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,8 @@ Bundler.require(*Rails.groups)
 module PasswordPusher
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
-    config.active_support.cache_format_version = 7.1
+    config.load_defaults 7.2
+    config.active_support.cache_format_version = 7.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
