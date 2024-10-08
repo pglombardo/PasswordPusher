d6b00f888de74730eb0fe56c5b29ae591c5d331c
Peter Giacomo Lombardo <pglombardo@hey.com>
2024-10-08 21:53:21 +0200

    Add SolidQueue and background jobs

---
+++ app/jobs/clean_up_pushes_job.rb
@@ -0,0 +1,75 @@
+class CleanUpPushesJob < ApplicationJob
+  queue_as :default
+
+  # Delete Anonymous Expired Pushes
+  #
+  # When a push expires, the payload is deleted but the metadata record still exists.  This
+  # includes information such as creation date, audit logs, duration etc..  When the record
+  # was created by an anonymous user, this data is no longer needed and we delete it (we
+  # don't want it).
+  #
+  # If a user attempts to retrieve a secret link that doesn't exist anymore, we still show
+  # the standard "This secret link has expired" message.  This strategy provides two benefits:
+  #
+  # 1. It hides the fact that if a secret ever exists or not (more secure)
+  # 2. It allows us to delete data that we don't want
+  #
+  # This task will run through all expired and anonymous records and delete them entirely.
+  #
+  # Because of the above, expired and anonymous secret URLs still will show the same
+  # expiration message
+  #
+  # Note: This applies to anonymous pushes.  For logged-in user records, we don't do this
+  # to maintain user audit logs.
+  #
+  def perform(*args)
+    # Log task start
+    logger.info("--> #{self.class.name}: Starting...")
+
+    counter = 0
+
+    Password.includes(:views)
+      .where(expired: true)
+      .where(user_id: nil)
+      .find_each do |push|
+      counter += 1
+      push.destroy
+    end
+
+    if Settings.enable_file_pushes
+      FilePush.includes(:views)
+        .where(expired: true)
+        .where(user_id: nil)
+        .find_each do |push|
+        counter += 1
+        push.destroy
+      end
+    end
+
+    if Settings.enable_url_pushes
+      Url.includes(:views)
+        .where(expired: true)
+        .where(user_id: nil)
+        .find_each do |push|
+        counter += 1
+        push.destroy
+      end
+    end
+
+    logger.info("  -> #{counter} total anonymous expired pushes deleted.")
+
+    # Log completion
+    logger.info("  -> #{self.class.name}: Finished.")
+  end
+
+  private
+
+  def logger
+    @logger ||= if ENV.key?(PWP_WORKER)
+      # We are running inside the pwpush-worker container.  Log to STDOUT
+      Logger.new($stdout)
+    else
+      Logger.new(Rails.root.join("log", "recurring.log"))
+    end
+  end
+end
