c079a0f8b3e8fc3f6acfe8c64015dd77942119d2
Peter Giacomo Lombardo <pglombardo@hey.com>
2024-11-06 09:27:35 +0100

    Allow Anonymous: Fix after sign up redirect path

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -78,6 +78,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
 
+  def after_inactive_sign_up_path_for(resource)
+    new_user_session_path
+  end
+
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
--- config/routes/users.rb
+++ config/routes/users.rb
@@ -1,15 +1,16 @@
-devise_for :users, skip: :registrations, controllers: {
-  sessions: "users/sessions",
-  passwords: "users/passwords",
-  unlocks: "users/unlocks",
-  confirmations: "users/confirmations"
-}
-
 allowed_reg_routes = if Settings.disable_signups
   %i[edit update]
 else
   %i[new create edit update]
 end
+
+devise_for :users, skip: :registrations, controllers: {
+  sessions: "users/sessions",
+  passwords: "users/passwords",
+  unlocks: "users/unlocks",
+  confirmations: "users/confirmations",
+  registrations: "users/registrations"
+}
 
 devise_scope :user do
   resource :registration,
@@ -18,8 +19,8 @@ devise_scope :user do
     path_names: {new: "sign_up"},
     controller: "users/registrations",
     as: :user_registration do
-    get :cancel
-    get :token
-    delete :token, action: :regen_token
-  end
+      get :cancel
+      get :token
+      delete :token, action: :regen_token
+    end
 end
