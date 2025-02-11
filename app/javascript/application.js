0c11c6264bbd936fa2751d917b953c7d6c8a16b2
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-02-11 15:00:56 +0100

    Themes: Fix JS mode switching (#3073)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,20 @@ import "@hotwired/turbo-rails"
 import "@rails/activestorage"
 import "bootstrap"
 import "./controllers"
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+// Function to handle theme changes
+const handleThemeChange = (e) => {
+    if (e.matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', 'light')
+    }
+}
+
+// Initial check
+handleThemeChange(prefersDarkScheme);
+
+// Listen for changes
+prefersDarkScheme.addEventListener('change', handleThemeChange);
