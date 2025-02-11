4c20f7456ccd938e14ed7617c43553655879b8f1
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-02-11 14:58:36 +0100

    Themes: Fix JS mode switching

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -9,21 +9,4 @@ window.Stimulus   = application
 console.log('Welcome to Password Pusher! ( ◑‿◑)ɔ┏🍟--🍔┑٩(^◡^ )')
 console.log(' --> 🏝 May all your pushes be stored securely, read once and expired quickly.')
 
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-// Function to handle theme changes
-const handleThemeChange = (e) => {
-    if (e.matches) {
-        document.documentElement.setAttribute('data-bs-theme', 'dark')
-    } else {
-        document.documentElement.setAttribute('data-bs-theme', 'light')
-    }
-}
-
-// Initial check
-handleThemeChange(prefersDarkScheme);
-
-// Listen for changes
-prefersDarkScheme.addEventListener('change', handleThemeChange);
-
 export { application }
