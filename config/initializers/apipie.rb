59ec5da1d7608834e7b209834ff34fa4d28fc8f0
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-02-12 16:57:47 +0100

    apipie docs: Add note on deprecated X-User-* headers

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -35,6 +35,8 @@ Apipie.configure do |config|
 
     The API has been updated to use Bearer tokens for authentication and a general cleanup of the API.
 
+    The X-User-Email and X-User-Token headers are deprecated although they will probably still be supported indefinitely for v1 of the API (and not carried into the eventual APIv2).
+
     Some clients may need minor updates if they were relying on non-standard behavior.
 
     If something has broken recently, please see this {pull request}[https://github.com/pglombardo/PasswordPusher/pull/3068] for more details.
