3df4a6e12e2e8a329fba49794760a052c2d7db52
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-02-18 15:09:59 +0100

    Add environment variable to disable Turbo Drive

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,11 @@ import "@hotwired/turbo-rails"
 import "@rails/activestorage"
 import "bootstrap"
 import "./controllers"
+
+// Check if Turbo Drive should be disabled
+const turboDriveEnabled = document.querySelector('meta[name="turbo-drive-enabled"]')?.content === 'true'
+
+if (!turboDriveEnabled) {
+  console.log("⚡ Turbo Drive is disabled via environment variable")
+  Turbo.session.drive = false
+}
