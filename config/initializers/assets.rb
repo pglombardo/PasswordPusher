1e623b0a3bcb59cfd29a12006a2c658b9808bb33
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-02-27 17:14:10 +0100

    Assets: Fix asset bundling & paths

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,17 +6,8 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join("vendor/stylesheets")
-Rails.application.config.assets.paths << Rails.root.join("vendor/stylesheets/@fontsource/roboto/files")
-Rails.application.config.assets.paths << Rails.root.join("vendor/stylesheets/@fontsource/roboto-slab/files")
-Rails.application.config.assets.paths << Rails.root.join("vendor/stylesheets/@fontsource/roboto-mono/files")
-
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons")
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
-
-Rails.application.config.assets.paths << Rails.root.join("node_modules/flag-icons")
-Rails.application.config.assets.paths << Rails.root.join("node_modules/flag-icons/css")
-Rails.application.config.assets.paths << Rails.root.join("node_modules/flag-icons/flags")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
