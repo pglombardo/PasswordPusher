bc7b43e21b152fa3148fcbcd8f0d4ffcbce64a29
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-02-27 14:16:03 +0100

    CSP: Updated nonce generator

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -19,7 +19,8 @@ Rails.application.configure do
     policy.connect_src :self, :https, :http, :ws, :wss
     policy.report_uri "/csp-violation-report"
   end
-
-  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  config.content_security_policy_nonce_directives = %w[script-src]
 end
+
+# Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies
+Rails.application.config.content_security_policy_nonce_generator = ->(request) { SecureRandom.base64(16) }
+Rails.application.config.content_security_policy_nonce_directives = %w[script-src]
