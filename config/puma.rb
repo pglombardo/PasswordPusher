ff7b8e6458c95b06a08bba802cbf7b57fbbe6245
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-04-17 20:27:28 +0200

    Memory: Run Puma in single mode

--- config/puma.rb
+++ config/puma.rb
@@ -8,9 +8,7 @@ environment rails_env
 
 case rails_env
 when "production"
-  # Default to 2 workers.  To override, set the WEB_CONCURRENCY environment variable
-  workers_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
-  workers workers_count if workers_count > 1
+  workers Integer(ENV.fetch("WEB_CONCURRENCY") { 0 })
 
   preload_app!
 when "development"
