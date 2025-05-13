1da01239213e4335147b469d89356061dd9c5abc
Hasan Özovalı <ozovalihasan@gmail.com>
2025-05-14 00:40:57 +0300

    Update test of FilePush model

--- test/models/file_push_test.rb
+++ test/models/file_push_test.rb
@@ -1,29 +1,36 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "active_storage/engine"
 
 class FilePushTest < ActiveSupport::TestCase
+  include ActionDispatch::TestProcess
+
   setup do
     @user = users(:luca)
   end
   test "should create file push with name" do
     file_push = FilePush.new(
-      payload: "test payload",
       name: "Test File Push",
       user: @user
     )
+    file = fixture_file_upload("monkey.png", "image/jpeg")
+    file_push.files.attach(file)
+
     assert file_push.save
     assert_equal "Test File Push", file_push.name
   end
   
   test "should include name in json representation" do
     file_push = FilePush.new(
-      payload: "test payload",
       name: "Test File Push",
       user: @user,
       expire_after_days: 7,
       expire_after_views: 10
     )
+    file = fixture_file_upload("monkey.png", "image/jpeg")
+    file_push.files.attach(file)
+
     assert file_push.save
     
     json = JSON.parse(file_push.to_json({}))
@@ -32,20 +39,24 @@ class FilePushTest < ActiveSupport::TestCase
 
   test "should save file push without name" do
     file_push = FilePush.new(
-      payload: "test payload",
       user: @user
     )
+    file = fixture_file_upload("monkey.png", "image/jpeg")
+    file_push.files.attach(file)
+
     assert file_push.save
     assert_nil file_push.name
   end
 
   test "should include name as nil in json representation" do
     file_push = FilePush.new(
-      payload: "test payload",
       user: @user,
       expire_after_days: 7,
       expire_after_views: 10
     )
+    file = fixture_file_upload("monkey.png", "image/jpeg")
+    file_push.files.attach(file)
+    
     assert file_push.save
     
     json = JSON.parse(file_push.to_json({}))
