8afa58b64e70459af940a9317cd8bb70f4b39e86
Hasan Özovalı <ozovalihasan@gmail.com>
2025-05-16 13:12:48 +0300

    Add validations and enum for AuditLog

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class AuditLog < ApplicationRecord
+  enum :kind, [:creation, :view, :failed_view, :expire, :failed_passphrase, :retrieve, :response, :close, :edit]
+
   belongs_to :push
   belongs_to :user, optional: true
+
+  validates :kind, presence: true
 end
