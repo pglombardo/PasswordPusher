16bf5b544773499448dd5a3ba1782e20d58232c9
Hasan Özovalı <ozovalihasan@gmail.com>
2025-05-17 22:19:05 +0300

    Update name of an action of PushesController

--- config/routes/pushes.rb
+++ config/routes/pushes.rb
@@ -1,9 +1,10 @@
 constraints(format: :html) do
-  resources :p, controller: :pushes, as: :pushes, except: %i[edit update] do
+  resources :p, controller: :pushes, as: :pushes, except: %i[edit update destroy] do
     get "preview", on: :member
     get "print_preview", on: :member
     get "passphrase", on: :member
     post "access", on: :member
+    delete "expire", on: :member
     get "r", on: :member, as: "preliminary", action: "preliminary"
     get "audit", on: :member
   end
