92ba6ccccab5ab1f3d460ef0ca9c18c381ee681b
Hasan Özovalı <ozovalihasan@gmail.com>
2025-05-20 12:44:17 +0300

    Refactor the logic used to log events

--- app/controllers/pushes_controller.rb
+++ app/controllers/pushes_controller.rb
@@ -4,6 +4,7 @@ require "securerandom"
 
 class PushesController < BaseController
   include SetPushAttributes
+  include LogEvents
   
   before_action :set_push, except: %i[new create index]
 
@@ -278,48 +279,6 @@ class PushesController < BaseController
 
 
   private
-
-  ##
-  # log_view
-  #
-  # Record that a view is being made for a push
-  #
-  def log_view(push)
-    if push.expired
-      log_event(push, :failed_view)
-    else
-      log_event(push, :view)
-    end
-    push
-  end
-
-  def log_creation(push)
-    log_event(@push, :creation)
-  end
-
-  def log_failed_passphrase(push)
-    log_event(push, :failed_passphrase)
-  end
-
-  def log_expire(push)
-    log_event(push, :expire)
-  end
-  
-  def log_event(push, kind)
-    ip = request.env["HTTP_X_FORWARDED_FOR"].nil? ? request.env["REMOTE_ADDR"] : request.env["HTTP_X_FORWARDED_FOR"]
-
-    # Limit retrieved values to 256 characters
-    user_agent = request.env["HTTP_USER_AGENT"].to_s[0, 255]
-    referrer = request.env["HTTP_REFERER"].to_s[0, 255]
-
-    if kind == :creation
-      push.audit_logs.build(kind: kind, user: current_user, ip:, user_agent:, referrer:)
-    else
-      push.audit_logs.create(kind: kind, user: current_user, ip:, user_agent:, referrer:)
-    end
-    nil
-  end
-
 
   def set_push
     @push = Push.includes(:audit_logs).find_by!(url_token: params[:id])
