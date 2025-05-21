89dbaa07b4c41afb0a3015fcc3da5b59b0205786
Hasan Özovalı <ozovalihasan@gmail.com>
2025-05-21 12:43:31 +0300

    Add a validation for audit logs

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -3,10 +3,10 @@
 class AuditLog < ApplicationRecord
   enum :kind, [:creation, :view, :failed_view, :expire, :failed_passphrase]
   
+  validates :kind, inclusion: { in: self.kinds, message: "%{value} is not a valid audit log type" }
+
   belongs_to :push
   belongs_to :user, optional: true
-
-  validates :kind, presence: true
 
   def subject_name
     user&.email || "❓"
