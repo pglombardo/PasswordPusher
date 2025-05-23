7fb4b97a69065c274fce20ebce6b6ccc34f5642e
Hasan Özovalı <ozovalihasan@gmail.com>
2025-05-24 01:27:20 +0300

    Update a comment of a migration

--- db/migrate/20250523202037_make_kind_required_for_audit_logs.rb
+++ db/migrate/20250523202037_make_kind_required_for_audit_logs.rb
@@ -1,6 +1,6 @@
 class MakeKindRequiredForAuditLogs < ActiveRecord::Migration[7.2]
   def change
-    # Make kind required for pushes
+    # Make kind required for audit logs
     change_column_null :audit_logs, :kind, false
   end
 end
