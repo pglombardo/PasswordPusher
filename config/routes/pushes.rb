b074914c0134484720841a631f670a5e093407a1
Hasan Özovalı <ozovalihasan@gmail.com>
2025-05-26 01:03:06 +0300

    Add redirects to support old push links having `/f/` and `/r/`

--- config/routes/pushes.rb
+++ config/routes/pushes.rb
@@ -1,4 +1,12 @@
 constraints(format: :html) do
+  get "/f/:url_token", to: redirect("/p/%{url_token}")
+  get "/f/:url_token/r", to: redirect("/p/%{url_token}/r")
+  get "/f/:url_token/passphrase", to: redirect("/p/%{url_token}/passphrase")
+
+  get "/r/:url_token", to: redirect("/p/%{url_token}")
+  get "/r/:url_token/r", to: redirect("/p/%{url_token}/r")
+  get "/r/:url_token/passphrase", to: redirect("/p/%{url_token}/passphrase")
+  
   resources :p, controller: :pushes, as: :pushes, except: %i[edit update destroy] do
     get "preview", on: :member
     get "print_preview", on: :member
