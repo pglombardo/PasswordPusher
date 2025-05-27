dde42ce0e5a6183b62ea3c640d4ead7425fef8ba
Hasan Özovalı <ozovalihasan@gmail.com>
2025-05-27 16:21:37 +0300

    Fix the name of a test used to test PushesController#index

--- test/integration/password/password_index_test.rb
+++ test/integration/password/password_index_test.rb
@@ -2,7 +2,7 @@
 
 require "test_helper"
 
-class PasswordActiveTest < ActionDispatch::IntegrationTest
+class PasswordIndexTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
@@ -53,7 +53,7 @@ class PasswordActiveTest < ActionDispatch::IntegrationTest
     # Since this is an active push, both Preview and Audit buttons should be present
     assert_select "div[aria-label='Push Controls']", 1 do |controls|
       assert_select controls.first, "a", 2 # Should have 2 buttons (Preview and Audit)
-      
+
       # Check the text content of the buttons
       assert_select controls.first, "a", text: "Preview", count: 1
       assert_select controls.first, "a", text: "Audit", count: 1
@@ -63,19 +63,19 @@ class PasswordActiveTest < ActionDispatch::IntegrationTest
     delete expire_push_path(@luca.pushes.last)
     assert_response :redirect
     follow_redirect!
-    
+
     # Verify the push is now expired
     assert @luca.pushes.last.expired
-    
+
     # Check the expired pushes list
     get pushes_path(filter: "expired")
     assert_response :success
-    
-     # Verify the push controls buttons
+
+    # Verify the push controls buttons
     # Since this is an expired push, only Audit button should be present
     assert_select "div[aria-label='Push Controls']", 1 do |controls|
       assert_select controls.first, "a", 1 # Should have 1 buttons (Audit)
-      
+
       # Check the text content of the buttons
       assert_select controls.first, "a", text: "Audit", count: 1
     end
