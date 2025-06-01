f6ba328be618650eb95d60104d028f2331a3ed8c
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-06-01 20:24:57 +0200

    DataMigration: Only migrate data if there is data

--- db/migrate/20250519010827_migrate_data_to_push_model.rb
+++ db/migrate/20250519010827_migrate_data_to_push_model.rb
@@ -1,31 +1,36 @@
 class MigrateDataToPushModel < ActiveRecord::Migration[7.2]
   def up
-    # Create migration status table if it doesn't exist
-    unless table_exists?(:data_migration_statuses)
-      create_table :data_migration_statuses do |t|
-        t.string :name, null: false
-        t.boolean :completed, default: false
-        t.datetime :completed_at
-        t.timestamps
-      end
-      add_index :data_migration_statuses, :name, unique: true
+    create_table :data_migration_statuses do |t|
+      t.string :name, null: false
+      t.boolean :completed, default: false
+      t.datetime :completed_at
+      t.timestamps
     end
+    add_index :data_migration_statuses, :name, unique: true
 
-    # Create initial status record
-    DataMigrationStatus.create_or_find_by(name: "push_model_migration").update!(
-      completed: false,
-      completed_at: nil
-    )
+    if (Password.count + FilePush.count + Url.count) > 0
+      # Create initial status record
+      DataMigrationStatus.create_or_find_by(name: "push_model_migration").update!(
+        completed: false,
+        completed_at: nil
+      )
 
-    # Start the background data migration job
-    MigrateDataToPushModelJob.perform_later
+      # Start the background data migration job
+      MigrateDataToPushModelJob.perform_later
+    else
+      # No data to migrate
+      DataMigrationStatus.create_or_find_by(name: "push_model_migration").update!(
+        completed: true,
+        completed_at: Time.current
+      )
+    end
   end
 
   def down
     attach_files_to_old_records
     remove_all_audit_logs
     remove_all_pushes
-    drop_table :data_migration_statuses if table_exists?(:data_migration_statuses)
+    drop_table :data_migration_statuses
   end
 
   private
@@ -39,7 +44,7 @@ class MigrateDataToPushModel < ActiveRecord::Migration[7.2]
   end
 
   def attach_files_to_old_records
-    Push.where(kind: Push.kinds[:file]).find_each do |push|
+    Push.where(kind: Push.kinds[:file], expired: false).find_each do |push|
       file_push = FilePush.find_by(url_token: push.url_token)
       if file_push
         push.files.attachments.each do |attachment|
