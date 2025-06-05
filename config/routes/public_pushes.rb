201a2f802600dc3858d48540fb53848ec8cd8a91
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-06-05 13:48:43 +0200

    Public Gateway: Add legacy route redirects (#3428)

--- config/routes/public_pushes.rb
+++ config/routes/public_pushes.rb
@@ -1,11 +1,16 @@
+# File pushes redirects - with query string preservation
+get "/f/:url_token", to: redirect(path: "/p/%{url_token}", status: 301)
+get "/f/:url_token/r", to: redirect(path: "/p/%{url_token}/r", status: 301)
+get "/f/:url_token/passphrase", to: redirect(path: "/p/%{url_token}/passphrase", status: 301)
+
+# URL pushes redirects - with query string preservation
+get "/r/:url_token", to: redirect(path: "/p/%{url_token}", status: 301)
+get "/r/:url_token/r", to: redirect(path: "/p/%{url_token}/r", status: 301)
+get "/r/:url_token/passphrase", to: redirect(path: "/p/%{url_token}/passphrase", status: 301)
+
 resources :p, controller: :pushes, as: :pushes, except: %i[edit update new create destroy] do
-  # get "preview", on: :member
-  # get "print_preview", on: :member
   get "passphrase", on: :member
   post "access", on: :member
   get "r", on: :member, as: "preliminary", action: "preliminary"
   delete "expire", on: :member
-  # get "audit", on: :member
-  # get "active", on: :collection
-  # get "expired", on: :collection
 end
