a332d1b20eca22104c5757470fac25ae5cb2eccc
Hasan Özovalı <ozovalihasan@gmail.com>
2025-07-04 14:38:43 +0300

    Update usage of Settings.host_protocol and Settings.host_domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -128,8 +128,8 @@ Rails.application.configure do
     config.action_mailer.raise_delivery_errors = Settings.mail.raise_delivery_errors
 
     config.action_mailer.default_url_options = {
-      host: Settings.host_domain,
-      protocol: Settings.host_domain.present? ? "https" : "http"
+      host: ENV["TLS_DOMAIN"] || Settings.host_domain,
+      protocol: (ENV["TLS_DOMAIN"].present? || Settings.host_protocol) ? "https" : "http"
     }
 
     config.action_mailer.smtp_settings = {
