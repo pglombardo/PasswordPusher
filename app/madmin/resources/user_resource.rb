7b7254b235cbe8d7b6696129b7a9f578d656c661
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-11-15 16:53:59 +0100

    Admin: Better new user form (#3873)
    * Admin: Better new user form
    
    * Disable User editing

--- app/madmin/resources/user_resource.rb
+++ app/madmin/resources/user_resource.rb
@@ -36,7 +36,9 @@ class UserResource < Madmin::Resource
   # end
 
   # Customize the display name of records in the admin area.
-  # def self.display_name(record) = record.name
+  def self.display_name(record)
+    record.email
+  end
 
   # Customize the default sort column and direction.
   # def self.default_sort_column = "created_at"
