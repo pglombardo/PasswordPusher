92ffb0276e48a8d601a856b9b584bc2717514308
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-12-05 22:20:33 +0100

    Various Cleanup: Remove unnecessary files from Docker image (#3961)
    * Remove legacy View model
    
    * Dockerfile: Remove unnecessary files from image
    
    * Remove more view resources

--- app/models/view.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class View < ApplicationRecord
-  belongs_to :password, optional: true
-  belongs_to :file_push, optional: true
-  belongs_to :url, optional: true
-  belongs_to :user, optional: true
-end
