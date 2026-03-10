f1b1f636d5418cb0724ec735e2277a9730f6b34c
Peter Giacomo Lombardo <pglombardo@hey.com>
2026-03-10 12:41:54 +0100

    Renamed option; better docker compose file

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :timeoutable and :omniauthable
   # Email-based modules (:confirmable, :lockable, :recoverable) are added when
-  # Settings.enable_email_devise_modules is true (requires SMTP in config/settings.yml).
+  # Settings.enable_user_account_emails is true (requires SMTP in config/settings.yml).
   devise_modules = [:database_authenticatable, :registerable, :rememberable, :validatable, :trackable, :timeoutable]
-  devise_modules += [:confirmable, :lockable, :recoverable] if Settings.enable_email_devise_modules
+  devise_modules += [:confirmable, :lockable, :recoverable] if Settings.enable_user_account_emails
   devise(*devise_modules)
 
   has_many :pushes, dependent: :destroy
