bfa55c5b6ddfb3eebddd6f4c0e917fd583e7ca6e
Peter Giacomo Lombardo <pglombardo@hey.com>
2026-03-19 13:40:41 +0100

    Fix import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,9 +3,8 @@ import "@rails/activestorage"
 import "./controllers"
 import "bootstrap"
 
-
 import LocalTime from "local-time"
-import { setLocalTimeLocaleFromDocument } from "local_time_locales"
+import { setLocalTimeLocaleFromDocument } from "./local_time_locales"
 
 // Check if Turbo Drive should be disabled
 const turboDriveEnabled = document.querySelector('meta[name="turbo-drive-enabled"]')?.content === 'true'
