51f25978f9d818630206351d65e45deb8e60c729
LukasMalyszko <lukas.malyszko@gmail.com>
2026-03-24 13:08:10 +0100

    Refactor controller registrations in index.js
    Updated the order of controller registrations in the index.js file for improved organization. The 'gdpr' and 'pwgen' controllers were repositioned, ensuring a clearer structure and consistency in the registration process.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,13 +10,13 @@ import SelectDropdownController from "./select_dropdown_controller"
 import ThemeController from "./theme_controller"
 import { application } from "./application"
 
+application.register("gdpr", GdprController)
 application.register("copy", CopyController)
 application.register("countdown", CountdownController)
-application.register("form", FormController)
-application.register("gdpr", GdprController)
-application.register("knobs", KnobsController)
-application.register("multi-upload", MultiUploadController)
-application.register("passwords", PasswordsController)
 application.register("pwgen", PWGenController)
+application.register("form", FormController)
+application.register("knobs", KnobsController)
+application.register("passwords", PasswordsController)
+application.register("multi-upload", MultiUploadController)
 application.register("pwpush--select-dropdown", SelectDropdownController)
 application.register("theme", ThemeController)
