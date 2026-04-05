f7c94f71aa6d2b3504a16710721fb1b53536b8df
Hasan Özovalı <ozovalihasan@gmail.com>
2026-04-06 01:07:29 +0300

    Update tests to run in parallel

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,6 +25,9 @@ ENV.keys.each do |key|
 end
 
 class ActiveSupport::TestCase
+  # Run tests in parallel with half of available processors
+  parallelize(workers: [Etc.nprocessors / 2, 1].max)
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
@@ -53,3 +56,8 @@ module ActionDispatch
     include Devise::Test::IntegrationHelpers
   end
 end
+
+ActiveSupport::Testing::Parallelization.after_fork_hooks << lambda { |worker_number|
+  system_test_port = 45678 + worker_number.to_i
+  ENV["SYSTEM_TEST_PORT"] = system_test_port.to_s
+}
