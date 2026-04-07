9d4170436393242463ba70d4ed6d11ea4448411c
Hasan Özovalı <ozovalihasan@gmail.com>
2026-04-07 03:33:17 +0300

    Update an environment variable used to detect test worker numbers

--- test/system/first_run_test.rb
+++ test/system/first_run_test.rb
@@ -12,7 +12,7 @@ class FirstRunTest < ApplicationSystemTestCase
   # Stub the boot code file to ensure that all parallelization tests are run independently
   def stub_boot_code_file
     boot_code_file = FirstRunBootCode::BOOT_CODE_FILE.dup
-    boot_code_file = boot_code_file.sub(".txt", "_#{ENV["TEST_PORT"]}.txt")
+    boot_code_file = boot_code_file.sub(".txt", "_#{ENV.fetch("TEST_WORKER_NUMBER", "")}.txt")
 
     stub_const(FirstRunBootCode, :BOOT_CODE_FILE, boot_code_file) do
       yield
