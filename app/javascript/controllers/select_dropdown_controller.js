d371eb21bf384d80e12b990dbf87d5e1613454f2
Hasan Özovalı <ozovalihasan@gmail.com>
2026-04-26 20:29:59 +0300

    Fix unselected option issue of notify_by_email locale

--- app/javascript/controllers/select_dropdown_controller.js
+++ app/javascript/controllers/select_dropdown_controller.js
@@ -3,9 +3,26 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "menuButtonInner", "field" ]
 
+  connect() {
+    this.restoreSelectedValue();
+  }
+
   select(event) {
     this.fieldTarget.value = event.currentTarget.dataset.lang;
     const template = event.currentTarget.querySelector('template');
     this.menuButtonInnerTarget.innerHTML = template.innerHTML
+  }
+
+  restoreSelectedValue() {
+    const selectedLang = this.fieldTarget.value;
+    if (selectedLang) {
+      const selectedOption = this.element.querySelector(`[data-lang="${selectedLang}"]`);
+      if (selectedOption) {
+        const template = selectedOption.querySelector('template');
+        if (template) {
+          this.menuButtonInnerTarget.innerHTML = template.innerHTML;
+        }
+      }
+    }
   }
 }
