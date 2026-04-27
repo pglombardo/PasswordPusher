7b38c23d894d95118b217478f08e521655895a92
Hasan Özovalı <ozovalihasan@gmail.com>
2026-04-27 22:04:39 +0300

    Update the timestamp of a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_27_123959) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_27_190300) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.integer "blob_id", null: false
     t.datetime "created_at", precision: nil, null: false
