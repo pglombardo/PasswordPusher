9fda21962d1c6845c8b9469f4f47e3544981a1af
Peter Giacomo Lombardo <pglombardo@hey.com>
2026-05-01 13:04:34 +0200

    Add local_time support to Data Explorer (madmin)
    Override madmin gem's default date_time and date field partials to display
    timestamps in the user's local timezone instead of raw UTC.
    
    Changes:
    - Created madmin/fields/date_time/_index.html.erb with local_time :short format
    - Created madmin/fields/date_time/_show.html.erb with local_time :long format
    - Created madmin/fields/date/_index.html.erb with local_date :short format
    - Created madmin/fields/date/_show.html.erb with local_date :long format
    - Added LocalTimeHelper to Madmin::ApplicationController to make helpers available
    
    This ensures timestamps in the Data Explorer (created_at, updated_at,
    last_sign_in_at, etc.) are displayed in the browser's local timezone,
    consistent with other parts of the application.

--- app/controllers/madmin/application_controller.rb
+++ app/controllers/madmin/application_controller.rb
@@ -1,6 +1,7 @@
 module Madmin
   class ApplicationController < Madmin::BaseController
     include SetLocale
+    helper LocalTimeHelper
 
     before_action :authenticate_user!
     before_action :authenticate_admin_user
