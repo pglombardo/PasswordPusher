f37876180da6443ba8ba403acb76e2a45c3c1d86
Hasan Özovalı <ozovalihasan@gmail.com>
2026-05-24 18:55:13 +0300

    Update versions of bundler and JavaScript packages (#4492)
    * Update Bundler version
    
    * Update versions of JavaScript packages

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,15 +6,15 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "@rails/activestorage", to: "@rails--activestorage.js" # @8.1.200
+pin "@rails/activestorage", to: "@rails--activestorage.js" # @8.1.300
 pin "bootstrap" # @5.3.8
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@fontsource/roboto", to: "@fontsource--roboto.js" # @5.2.9
-pin "@fontsource/roboto-mono", to: "@fontsource--roboto-mono.js" # @5.2.8
+pin "@fontsource/roboto", to: "@fontsource--roboto.js" # @5.2.10
+pin "@fontsource/roboto-mono", to: "@fontsource--roboto-mono.js" # @5.2.9
 pin "@fontsource/roboto-slab", to: "@fontsource--roboto-slab.js" # @5.2.8
-pin "@rails/actioncable", to: "@rails--actioncable.js" # @8.1.200
+pin "@rails/actioncable", to: "@rails--actioncable.js" # @8.1.300
 pin "clipboard" # @2.0.11
-pin "js-cookie" # @3.0.5
+pin "js-cookie" # @3.0.7
 pin "omgopass" # @3.2.1
 pin "omgopass/random.js", to: "omgopass--random.js.js" # @3.2.1
 pin "local-time" # @3.0.3
