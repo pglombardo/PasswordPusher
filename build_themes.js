3a7ba91fbdcf23013cd434f2dda0c4e0c352bd65
Hasan Özovalı <ozovalihasan@gmail.com>
2026-05-25 00:54:52 +0300

    Remove unnecessary copy-pasting function

--- build_themes.js
+++ build_themes.js
@@ -19,15 +19,6 @@ function themeSlugs() {
     .sort();
 }
 
-function copyBootstrapIconFonts() {
-  const srcDir = path.join(root, "node_modules/bootstrap-icons/font/fonts");
-  const destDir = path.join(buildsDir, "fonts");
-  fs.mkdirSync(destDir, { recursive: true });
-  for (const name of fs.readdirSync(srcDir)) {
-    fs.copyFileSync(path.join(srcDir, name), path.join(destDir, name));
-  }
-}
-
 function run(cmd, extraEnv = {}) {
   execSync(cmd, {
     cwd: root,
@@ -53,8 +44,6 @@ function restoreDefaultSelected() {
 
 const singleMode =
   process.argv.includes("--single") || process.env.BUILD_CSS_SINGLE === "1";
-
-copyBootstrapIconFonts();
 
 if (singleMode) {
   const slug = (process.env.PWP__THEME || "default").toLowerCase();
