787ef77efe330c06adb78a00c0247389efaa470d
Peter Giacomo Lombardo <pglombardo@hey.com>
2026-07-04 13:24:06 +0200

    Update notifications to match Pro

--- app/mailers/push_created_mailer.rb
+++ app/mailers/push_created_mailer.rb
@@ -11,8 +11,8 @@ class PushCreatedMailer < ApplicationMailer
 
     I18n.with_locale(locale || I18n.default_locale) do
       @secret_url = mailer_secret_push_url(@push, params[:locale])
-      sender = @push.user&.email.presence || _("Someone")
-      @subject = "#{sender} #{_("has sent you a push")}"
+      @sender = @push.user&.email.presence || _("Someone")
+      @subject = _("You have received a push")
       mail(
         to: params[:recipient],
         subject: @subject
